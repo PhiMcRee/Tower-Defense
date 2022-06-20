6323ba7522ad54f77572e4ac8162de1eed59331b
Phi McRee <phionamcree@gmail.com>
2022-06-20 15:46:57 -0600

    More First Breadth Pathing

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] List<Tile> path = new List<Tile>();
     [SerializeField] [Range(0f, 5f)] float speed = 0.5f;
 
     Enemy enemy;
@@ -29,18 +29,20 @@ public class EnemyMovement : MonoBehaviour
     void FindPath()
     {
         path.Clear();
-        GameObject parent = GameObject.FindGameObjectWithTag("Path");
 
-        foreach (Transform child in parent.transform)
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Path");
+
+        foreach (GameObject tile in tiles)
         {
-            Waypoint waypoint = child.GetComponent<Waypoint>();
-            if(waypoint != null)
-              {
-              path.Add(waypoint);
-              }
-        }
+            Tile waypoint = tile.GetComponent<Tile>();
 
+            if (waypoint != null)
+            {
+                path.Add(waypoint);
+            }
+        }
     }
+
 
     void ReturnToStart()
     {
@@ -55,7 +57,7 @@ public class EnemyMovement : MonoBehaviour
 
     IEnumerator FollowPath()
     {
-        foreach (Waypoint waypoint in path)
+        foreach (Tile waypoint in path)
         {
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position;
